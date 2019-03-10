9201640edb5aa4bcdcf53e36022881341cd91b72
Juan Polanco <juanpolanco5008@gmail.com>
2019-03-10 11:52:45 -0400

    added more files

--- mini_blog/catalog/models.py
+++ mini_blog/catalog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+class post(models.Model):
+    authoer = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    text = models.TextField()
+    created_date = models.DateTimeField(default =timezone.now)
+    publish_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.publish_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
